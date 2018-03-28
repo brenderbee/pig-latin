c99d3ab4e5b9b8c7dd3439b1691d0b49551e8cc7
Brenda Franco and Garnett Puett <brenda@>
2018-03-28 14:38:46 -0700

    Add functionality for multi consonant strings and words beginning with qu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // Business Logic
 var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 var pigLatin = [];
+
 function translate(phrase) {
   for (var i = 0; i < vowels.length; i++) {
     if (phrase[0] === vowels[i] && phrase.length === 1) { //adds "ay" words beginning with a vowel
@@ -9,7 +10,38 @@ function translate(phrase) {
     } else if (phrase[0] === vowels[i]) {
       pigLatin.push(phrase.join(""), "way");
       return pigLatin.join("");
-    }
+    } else if (phrase[0] !== vowels[i] && phrase[1] === vowels[i]) {
+      for (var j = 0; j < phrase.length; j++) {
+        if (phrase[j] !== vowels[i]) {
+          var end = phrase.slice(1).join("");
+          pigLatin.push(end, phrase[0], "ay")
+          return pigLatin.join("");
+        }
+
+      }
+    } else if (phrase[0] !== vowels[i] && phrase[1] !== vowels[i]) {
+      for (var j = 0; j < phrase.length; j++) {
+        if (phrase[j] !== vowels[i]) {
+          var endTwo = phrase.slice(2).join("");
+          pigLatin.push(endTwo, phrase[0], phrase[1], "ay")
+          return pigLatin.join("");
+
+        }
+      }
+    } else if (phrase[0] !== vowels[i] && phrase[1] !== vowels[i] && phrase[2] !== vowels[i]) {
+        for (var j = 0; j < phrase.length; j++) {
+          if (phrase[j] !== vowels[i]) {
+            var endThree = phrase.slice(3).join("");
+            pigLatin.push(endThree, phrase[0], phrase[1], phrase[2], "ay")
+            return pigLatin.join("");
+
+        }
+      }
+    } else if (phrase[0] === "q" && phrase[1] === "u") {
+        var qend = phrase.slice(2).join("")
+        pigLatin.push(qend, phrase[0], phrase[1], "ay")
+        return pigLatin.join("");
+    } 
   }
 }
 
@@ -22,6 +54,7 @@ $(document).ready(function() {
     var inputLetters = inputSentence.split("");
     var output = translate(inputLetters);
 
+    // $("#output").text(output)
     console.log(output);
   });
 });
