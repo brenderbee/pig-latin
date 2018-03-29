8bf6cc1800e1936eefdd41891b63d8b899838997
Brenda Franco and Garnett Puett <garnett@>
2018-03-28 17:37:58 -0700

    Refactor consecutive consonants logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,38 @@
 // Business Logic
-var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "y", "Y"];
 var pigLatin = [];
+var consonants = [];
 
 function translate(phrase) {
+// words beginning in vowels or qu
   for (var i = 0; i < vowels.length; i++) {
-    if (phrase[0] === vowels[i] && phrase.length === 1) { //adds "ay" words beginning with a vowel
+    if (phrase[0] === vowels[i] && phrase.length === 1) {
       pigLatin.push(phrase[0], "ay");
       return pigLatin.join("");
     } else if (phrase[0] === vowels[i]) {
       pigLatin.push(phrase.join(""), "way");
       return pigLatin.join("");
-    } else if (phrase[0] !== vowels[i] && phrase[1] === vowels[i]) {
-      for (var j = 0; j < phrase.length; j++) {
-        if (phrase[j] !== vowels[i]) {
-          var end = phrase.slice(1).join("");
-          pigLatin.push(end, phrase[0], "ay")
-          return pigLatin.join("");
-        }
-
-      }
-    } else if (phrase[0] !== vowels[i] && phrase[1] !== vowels[i]) {
-      for (var j = 0; j < phrase.length; j++) {
-        if (phrase[j] !== vowels[i]) {
-          var endTwo = phrase.slice(2).join("");
-          pigLatin.push(endTwo, phrase[0], phrase[1], "ay")
-          return pigLatin.join("");
-
-        }
-      }
-    } else if (phrase[0] !== vowels[i] && phrase[1] !== vowels[i] && phrase[2] !== vowels[i]) {
-        for (var j = 0; j < phrase.length; j++) {
-          if (phrase[j] !== vowels[i]) {
-            var endThree = phrase.slice(3).join("");
-            pigLatin.push(endThree, phrase[0], phrase[1], phrase[2], "ay")
-            return pigLatin.join("");
-
-        }
-      }
     } else if (phrase[0] === "q" && phrase[1] === "u") {
         var qend = phrase.slice(2).join("")
         pigLatin.push(qend, phrase[0], phrase[1], "ay")
         return pigLatin.join("");
-    } 
+    } else {
+      break;
+    }
   }
-}
 
+// consecutive consonants
+for (var j = 0; j < phrase.length; j++) {
+    if (phrase[j].match(/[b-df-hj-np-tv-z]/)) {
+      consonants.push(phrase[j]);
+    } else {
+      var splicedPhrase = phrase.splice(j);
+      pigLatin.push(splicedPhrase.join(""), consonants.join(""), "ay");
+      return pigLatin.join("");
+      break;
+    }
+  }
+} //end of function translate()
 
 // User-Interface Logic
 $(document).ready(function() {
@@ -54,7 +42,7 @@ $(document).ready(function() {
     var inputLetters = inputSentence.split("");
     var output = translate(inputLetters);
 
-    // $("#output").text(output)
+    $("#output").text(output)
     console.log(output);
   });
 });
