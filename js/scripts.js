505bf7ee4baa849186eb739bbb52b06552549d68
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-03-31 13:53:34 -0700

    Fix bug that caused vowel starting words to repeat.

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,24 @@ debugger;
     letters = word.split("");
 
 // Words beginning with vowels or qu
-    // if (letters[0].match(/[aeiouyAEIOUY]/) && letters.length === 1) {
-    //   pigLatin.push(letters[0], "ay", " ");
-    // } else if (letters[0].match(/[aeiouyAEIOUY]/)) {
-    //   pigLatin.push(word, "way", " ");
-    // } else if (letters[0].match(/[qQ]/) && letters[1].match(/[uU]/)) {
-    //   var qend = letters.slice(2).join("");
-    //   pigLatin.push(qend, letters[0], letters[1], "ay", " ");
-    // } console.log(pigLatin+);
-// Words beggining with consonants
-    for (var j = 0; j < letters.length; j++) {
-      if (letters[j].match(/[^aeiouyAEIOUY]/)) {
-        consonants.push(letters[j]);
-      } else {
-        var splicedPhrase = letters.splice(j);
-        pigLatin.push(splicedPhrase.join(""), consonants.join(""), "ay", " ");
-        consonants = [];
-        return pigLatin.join("");
-        break;
+    if (letters[0].match(/[aeiouyAEIOUY]/) && letters.length === 1) {
+      pigLatin.push(letters[0], "ay", " ");
+    } else if (letters[0].match(/[aeiouyAEIOUY]/)) {
+      pigLatin.push(word, "way", " ");
+    } else if (letters[0].match(/[qQ]/) && letters[1].match(/[uU]/)) {
+      var qend = letters.slice(2).join("");
+      pigLatin.push(qend, letters[0], letters[1], "ay", " ");
+    } else {
+      // Words beginning with consonants
+      for (var j = 0; j < letters.length; j++) {
+        if (letters[j].match(/[^aeiouyAEIOUY]/)) {
+          consonants.push(letters[j]);
+        } else {
+          var splicedPhrase = letters.splice(j);
+          pigLatin.push(splicedPhrase.join(""), consonants.join(""), "ay", " ");
+          consonants = [];
+          break;
+        }
       }
     }
   });
